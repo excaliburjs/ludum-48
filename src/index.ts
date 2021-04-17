8111bb812867f10d144575165774e8024680607f
Erik Onarheim <erik.onarheim@gmail.com>
2021-04-16 22:46:15 -0500

    Basic game building

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("hello world");
+import "regenerator-runtime/runtime"; // needed for async/await with parcel
+
+import { Engine, Flags, Loader } from "excalibur";
+import { Player } from "./player";
+import { Resources } from "./resources";
+
+Flags.enable("use-webgl");
+class Game extends Engine {
+  constructor() {
+    super({ canvasElementId: "game", width: 800, height: 600 });
+  }
+  initialize() {
+    const player = new Player();
+    this.add(player);
+
+    const loader = new Loader();
+    loader.addResource(Resources.Sword);
+    this.start(loader);
+  }
+}
+
+export const game = new Game();
+
+game.initialize();
---
+++ src/resources.ts
@@ -0,0 +1,8 @@
+import { Resource, Texture, Graphics } from "excalibur";
+import sword from "./images/sword.png";
+
+let Resources = {
+  Sword: new Graphics.ImageSource(sword),
+};
+
+export { Resources };
