aa8ac7188aae8d7278ff6431e58f68fb3b43bc96
Erik Onarheim <erik.onarheim@gmail.com>
2021-04-16 22:48:43 -0500

    Fix git sha

--- posthtml.config.js
+++ posthtml.config.js
@@ -3,7 +3,7 @@ module.exports = {
     "posthtml-expressions": {
       locals: {
         commitRef() {
-          return process.env.COMMIT_REF || "local";
+          return process.env.GITHUB_SHA || "local";
         },
       },
     },
