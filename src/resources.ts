63c44afd1f695aef4218e0dbdaf55c0f37bf3f8d
Kamran Ayub <kamran.ayub@gmail.com>
2021-04-24 14:28:21 -0500

    a bit better dirt tile base sprite

--- src/resources.ts
+++ src/resources.ts
@@ -1,13 +1,13 @@
 import { Resource, Texture, Graphics, Sound } from "excalibur";
 import sword from "./images/sword.png";
-import dirt from "./images/placeholder-dirt.png";
+import dirt from "./images/dirt_001.png";
 import digWav from "./sound/dig.wav";
 import digMp3 from "./sound/dig.mp3";
 
 let Resources = {
   Sword: new Graphics.ImageSource(sword),
   Dirt: new Graphics.ImageSource(dirt),
-  DigSound: new Sound(digMp3, digWav)
+  DigSound: new Sound(digMp3, digWav),
 };
 
 export { Resources };
