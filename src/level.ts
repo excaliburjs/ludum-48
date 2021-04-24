3eacce139701e1c73e311d04f6547cb0ee3922fb
Erik Onarheim <erik.onarheim@gmail.com>
2021-04-24 09:27:13 -0500

    More chunks

--- src/level.ts
+++ src/level.ts
@@ -2,10 +2,14 @@ import { Engine, Scene, TileMap, vec } from "excalibur";
 import { Resources } from "./resources";
 
 export class Level extends Scene {
+  start = 5; // tiles down
+  chunkHeight = 16; // full screen
   onInitialize(engine: Engine) {
-    const tileMap = this.generateChunk(64 * 5);
+    const tileMap = this.generateChunk(64 * this.start);
+    const tileMap2 = this.generateChunk(64 * (this.chunkHeight + this.start));
 
     this.add(tileMap);
+    this.add(tileMap2);
   }
 
   generateChunk(yPos: number): TileMap {
@@ -13,7 +17,7 @@ export class Level extends Scene {
     const tileMap = new TileMap({
       x: 0,
       y: yPos,
-      rows: 10,
+      rows: this.chunkHeight,
       cols: 9, // try 5 then 7 or 9?
       cellWidth: 64,
       cellHeight: 64,
