f0801b11e4b87261e9225c2c6692e0e7ecfa47d9
Kamran Ayub <kamran.ayub@gmail.com>
2021-04-24 20:27:00 -0500

    resources for tunnel:

--- src/resources.ts
+++ src/resources.ts
@@ -2,6 +2,8 @@ import { Graphics, Sound } from "excalibur";
 import sword from "./images/sword.png";
 import dirt from "./images/dirt_001.png";
 import dirtBackground from "./images/dirt_back.png";
+import dirtTunnel from "./images/dirt_tunnel.png";
+import dirtAngle from "./images/dirt_angle.png";
 import rock from "./images/rock_001.png";
 import digWav from "./sound/dig.wav";
 import digMp3 from "./sound/dig.mp3";
@@ -16,6 +18,8 @@ let Resources = {
   Sword: new Graphics.ImageSource(sword),
   Dirt: new Graphics.ImageSource(dirt),
   DirtBackground: new Graphics.ImageSource(dirtBackground),
+  DirtTunnel: new Graphics.ImageSource(dirtTunnel),
+  DirtAngle: new Graphics.ImageSource(dirtAngle),
   Rock: new Graphics.ImageSource(rock),
   DigSound: new Sound(digMp3, digWav),
   ClankSound: new Sound(clankMp3, clankWav),
